5402b60433066f1fcccbadd561a49f984eba45b9
Mehak-B <mehakbhardwaj2013@gmail.com>
2025-03-29 14:47:20 +0530

    working on polyfills

--- JavascriptProjects/TODO/LogicalQuestions/logic.js
+++ JavascriptProjects/TODO/LogicalQuestions/logic.js
@@ -20,24 +20,70 @@
 
 // }
 // console.log(sumConsecutiveArray(arr,4))
-let arr = [1,2,100,3,4,5,42,6,7,8,8,56,20,500]
+// let arr = [1,2,100,3,4,5,42,6,7,8,8,56,20,500]
 
-function secMAx(item){
-    let max = 0;
-    let secmax = 0
-    for(let i = 0 ; i < item.length; i++){
-            if(item[i]> max){
-                secmax = max
-                max = item[i]
-                console.log("max",max, secmax)    
+// function secMAx(item){
+//     let max = 0;
+//     let secmax = 0
+//     for(let i = 0 ; i < item.length; i++){
+//             if(item[i]> max){
+//                 secmax = max
+//                 max = item[i]
+//                 console.log("max",max, secmax)    
 
-}else if( item[i]>secmax){
-    secmax = item[i]
-    console.log("sec",secmax)
+// }else if( item[i]>secmax){
+//     secmax = item[i]
+//     console.log("sec",secmax)
 
-}
-    }
-    return secmax
-}
+// }
+//     }
+//     return secmax
+// }
 
-console.log(secMAx(arr))
+// console.log(secMAx(arr))
+
+
+// polyfill for splice
+
+// let arr = [2,5,3,7,54,4]
+
+
+// // index = 2
+// // delNum = 2
+// // 3,7
+// // [2,5,54,7]
+
+// Array.prototype.mySplice = function (index, delNum = null, ...addVal) {
+
+//     let delItem = [];
+//     let newArr = [...this];
+//     this.length = 0
+//     for( let i = 0; i<newArr.length; i++){
+//         if(i >= index && i<= index+delNum-1) {
+//          delItem.push(newArr[i]) 
+//          console.log("addVal",addVal)
+//         }else{
+//         this.push(newArr[i])
+//     }
+// }
+// // console.log(this.length + addVal.length)
+
+// const rightPart = this.slice(index)
+// this.length = index
+
+// for(let i=0;i<addVal.length ; i++){
+//             this[this.length] = addVal[i] 
+//             this[6] = addVal[0]
+// }
+// for(let i = 0; i<rightPart.length; i++){
+//     this[this.length] = rightPart[i]
+// }
+// console.log()
+//     return delItem
+// }
+
+// console.log(arr.mySplice(2,2,4,5,6,7,8,9))
+// console.log(arr)
+
+
+let arr = [2,5,3,7,54,4]
